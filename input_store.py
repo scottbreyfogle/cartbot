757e004fb36def72bf92712bd00c566bc19045d4
Scott Breyfogle <swb02009@mymail.pomona.edu>
2013-04-12 23:05:39 -0700

    Made input store multithreading and runnable

--- input_store.py
+++ input_store.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python2
 from evdev import InputDevice, categorize, ecodes
 from re import search
 from datetime import datetime
+import thread
+import pickle
 import time
 import os
+import sys
 import Image
 
 dev = InputDevice('/dev/input/event4')
@@ -19,13 +23,23 @@ def read_input():
             else:
                 if key in events:
  
-def store():
+def store(filename):
+    thread.start_new_thread(read_input, ())
     input_dict = {}
     previous_time = datetime.now()
     current_time = 0
-    while True:
-        current_time = datetime.now() - previous_time
-        os.system("scrot -u %(current_time)s.png", current_time)
-        current_img = Image.open("%(current_time)s.png", current_time)
-        input_dict[(current_img,events)] = current_time
-        time.sleep(0.1)
+    try:
+        while True:
+            current_time = datetime.now() - previous_time
+            os.system("scrot -u %(current_time)s.png", current_time)
+            current_img = Image.open("%(current_time)s.png", current_time)
+            input_dict[(current_img,events)] = current_time
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        json.dump(f, input_dict)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        store(sys.args[1])
+    else:
+        print "Usage: ./input_store.py file"
