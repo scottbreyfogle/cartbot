2e20c6ad80807ec26c106a3f0eccb399b593889d
Will Coster <willcoster@gmail.com>
2013-04-12 23:12:30 -0700

    Working train-net.py!
    Fixed a bug in net_to_key.

--- train-net.py
+++ train-net.py
@@ -3,7 +3,8 @@
 import network
 
 import sys
-from cPickle import (dump,load)
+import json
+import pickle
 import Image
 import evdev.ecodes
 from pybrain.tools.shortcuts import buildNetwork
@@ -20,7 +21,7 @@ if len(sys.argv) < 3:
 
 training_file = open(sys.argv[1], "r")
 neural_net_file = open(sys.argv[2], "w")
-hidden_nodes = 5
+hidden_nodes = 15
 if len(sys.argv) > 3 and int(sys.argv[3]) > 0:
     hidden_nodes =  int(sys.argv[3])
 
@@ -30,23 +31,27 @@ if len(sys.argv) > 3 and int(sys.argv[3]) > 0:
 
 net = buildNetwork(network.input_nodes, hidden_nodes, network.output_nodes)
 data_set = SupervisedDataSet(network.input_nodes, network.output_nodes)
-training_data = load(training_file)
+training_data = json.load(training_file)
 training_file.close()
 
 print("Creating data set...")
 
 for time in training_data:
     image_file, keys = training_data[time]
-    input_values = network.image_to_input(Image.load(image_file))
-    output_values = network.key_to_output(keys)
+    input_values = network.image_to_input(Image.open(image_file))
+    output_values = network.keys_to_output(keys)
     data_set.addSample(input_values, output_values)
+
+print(len(data_set))
 
 print("Training the network...")
 
 trainer = BackpropTrainer(net, data_set)
-trainer.trainUntilConvergence(None,None,True) # We want it to be verbose
+trainer.trainUntilConvergence() # We want it to be verbose
+#for i in xrange(1000):
+#    print("\t" + str(trainer.train()))
 
 print("Saving to disk...")
 
-dump(net,neural_net_file)
+pickle.dump(net,neural_net_file)
 
