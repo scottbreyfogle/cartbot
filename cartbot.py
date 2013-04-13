7cbf6ac9149ebbb303e2f214242c1815b973484c
Will Coster <willcoster@gmail.com>
2013-04-13 04:20:44 -0700

    Trying edge detecting...

--- cartbot.py
+++ cartbot.py
@@ -2,30 +2,32 @@
 import time
 import sys
 import pickle
+from subprocess import Popen
 from window import get_active_window
 from network import net_to_keys,image_to_input, net_mapping
 from evdev import uinput, ecodes
 
 def run(network):
-    emulator = Popen(["mupen64plus", "roms/Mario Kart 64 (USA).n64"])
-
     running = True
     ui = uinput.UInput()
     while running:
         vals = network.activate(image_to_input(get_active_window()))
         for (index, weight) in enumerate(vals):
             if weight > .5:
-                #print("PUSHING: " + str(net_mapping[index]))
                 ui.write(ecodes.EV_KEY, net_mapping[index], 1)
-                #ui.write(ecodes.EV_KEY, ecodes.KEY_ENTER, 1)
+#                if(net_mapping[index] == ecodes.KEY_LEFT or net_mapping[index] == ecodes.KEY_RIGHT):
+#                    ui.write(ecodes.EV_KEY, net_mapping[index], 0)
                 ui.syn()
+
             else:
                 #print("POPPING: " + str(net_mapping[index]))
                 ui.write(ecodes.EV_KEY, net_mapping[index], 0)
                 #ui.write(ecodes.EV_KEY, ecodes.KEY_ENTER, 0)
                 ui.syn()
-        if emulator.poll() != None:
-            running = False
+
+        time.sleep(.01)
+        ui.write(ecodes.EV_KEY, ecodes.KEY_LEFT, 0)
+        ui.write(ecodes.EV_KEY, ecodes.KEY_RIGHT, 0)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
