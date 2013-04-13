4962133ea13d78c754066a760dda07ac6d1becd8
Will Coster <willcoster@gmail.com>
2013-04-12 22:32:14 -0700

    Committing my local changes...

--- network.py
+++ network.py
@@ -2,13 +2,16 @@ import ImageOps
 import Image
 
 # How far to down sample the input image
-sampleWidth = 32
-sampleHeight = 24
+sample_width = 32
+sample_height = 24
+
+input_nodes = sample_width*sample_height
+output_nodes = 10
 
 def down_sample(image):
     """Takes and image and down scales and grayscales it so that each pixel
     corresponds to one input node."""
-    return ImageOps.grayscale(image.thumbnail((sampleWidth,sampleHeight), Image.ANTIALIAS))
+    return ImageOps.grayscale(image.thumbnail((sample_width,sample_height), Image.ANTIALIAS))
 
 def image_to_input(image):
     """Takes an image and turns it into a sequence of node input values"""
--- train-net.py
+++ train-net.py
@@ -3,39 +3,42 @@
 import network
 
 import sys
-import cpickle
+from cPickle import (dump,load)
 import Image
 import evdev.ecodes
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
-hidden_nodes = 5
-
 ################################################################################
 # Handle command line args
 ################################################################################
 
-if len(sys.argv) != 3:
-    print("Usage: train_data.pickle neural_net.pickle")
+if len(sys.argv) < 3:
+    print("Usage: train_data.pickle neural_net.pickle [hidden_nodes]")
+    exit(1)
 
-training_file = sys.argv[1]
-neural_net_file = sys.argv[2]
+training_file = open(sys.argv[1], "r")
+neural_net_file = open(sys.argv[2], "w")
+hidden_nodes = 5
+if len(sys.argv) > 3 and int(sys.argv[3]) > 0:
+    hidden_nodes =  int(sys.argv[3])
 
 ################################################################################
 # Main
 ################################################################################
 
-net = buildNetwork(sampleWidth*sampleHeight, hidden_nodes, output_nodes)
-data_set = SupervisedDataSet(sampleWidth*sampleHeight, output_nodes)
-training_data = pickle.load(training_file)
+net = buildNetwork(network.input_nodes, hidden_nodes, network.output_nodes)
+data_set = SupervisedDataSet(network.input_nodes, network.output_nodes)
+training_data = load(training_file)
+training_file.close()
 
 print("Creating data set...")
 
 for time in training_data:
     image_file, keys = training_data[time]
-    input_values = image_to_input(Image.load(image_file))
-    output_values = key_to_output(keys)
+    input_values = network.image_to_input(Image.load(image_file))
+    output_values = network.key_to_output(keys)
     data_set.addSample(input_values, output_values)
 
 print("Training the network...")
@@ -45,5 +48,5 @@ trainer.trainUntilConvergence(None,None,True) # We want it to be verbose
 
 print("Saving to disk...")
 
-pickle.dump(net,neural_net_file)
+dump(net,neural_net_file)
 
