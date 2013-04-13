15199054199dff6392871841526f46a6433588fd
Scott Breyfogle <swb02009@mymail.pomona.edu>
2013-04-13 05:34:35 -0700

    Multifile input

--- train-net.py
+++ train-net.py
@@ -6,26 +6,21 @@ import sys
 import json
 import pickle
 import Image
-from evdev import ecodes
+import evdev.ecodes
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.datasets import SupervisedDataSet, ImportanceDataSet
+from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
 ################################################################################
 # Handle command line args
 ################################################################################
 
-if len(sys.argv) < 5:
-    print("Usage: train_data.pickle neural_net.pickle start_time end_time [hidden_nodes]")
+if len(sys.argv) < 4:
+    print("Usage: hidden_nodes neural_net.pickle train_data.json[+]")
     exit(1)
 
-training_file = open(sys.argv[1], "r")
 neural_net_file = open(sys.argv[2], "w")
-start_time = float(sys.argv[3])
-end_time = float(sys.argv[4])
-hidden_nodes = 15
-if len(sys.argv) > 5 and int(sys.argv[5]) > 0:
-    hidden_nodes =  int(sys.argv[5])
+hidden_nodes = int(sys.argv[1])
 
 ################################################################################
 # Main
@@ -33,29 +28,26 @@ if len(sys.argv) > 5 and int(sys.argv[5]) > 0:
 
 net = buildNetwork(network.input_nodes, hidden_nodes, network.output_nodes)
 data_set = SupervisedDataSet(network.input_nodes, network.output_nodes)
-#data_set = ImportanceDataSet(network.input_nodes, network.output_nodes)
-training_data = json.load(training_file)
-training_file.close()
 
 print("Creating data set...")
 
-for time in training_data:
-    if float(time) > start_time and float(time) < end_time:
+for train_file in sys.argv[3:]:
+    train_file = open(train_file,'r')
+    training_data = json.load(train_file)
+    train_file.close()
+    for time in training_data:
         image_file, keys = training_data[time]
         input_values = network.image_to_input(Image.open(image_file))
         output_values = network.keys_to_output(keys)
-        importance = 1
-        if output_values[network.key_mapping[ecodes.KEY_LEFT]] or output_values[network.key_mapping[ecodes.KEY_RIGHT]]: 
-            importance = 4
-        #data_set.addSample(input_values, output_values, importance)
-        for i in xrange(importance):
-            data_set.addSample(input_values, output_values)
+        data_set.addSample(input_values, output_values)
+
+print(len(data_set))
 
 print("Training the network...")
 
 trainer = BackpropTrainer(net, data_set)
 #trainer.trainUntilConvergence() # We want it to be verbose
-for i in xrange(50):
+for i in xrange(250):
     print("\t" + str(trainer.train()))
 
 print("Saving to disk...")
