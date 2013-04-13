8f425ddbffa47fb8d72ead2b1b775589468f905f
Michael Vassilevsky <mvassilevsky@gmail.com>
2013-04-12 22:33:43 -0700

    Made minor adjustments to string formatting.

--- input_store.py
+++ input_store.py
@@ -6,8 +6,8 @@ import Image
 input_dict = {}
 current_time = 0
 while True:
-    os.system("scrot -u %s.png", current_time)
-    current_img = Image.open("%s.png", current_time)
+    os.system("scrot -u %(current_time)s.png", current_time)
+    current_img = Image.open("%(current_time)s.png", current_time)
     input_dict[(current_img,REPLACE_ME_WITH_NAME_OF_SET)] = current_time #replace REPLACE_ME-WITH_NAME_OF_SET with the name of the set of keyboard actions
     current_time += 0.5
     time.sleep(0.5)
