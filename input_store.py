a0dc645d402b80ec0a3d4250380af5da8a262bc1
Will Coster <willcoster@gmail.com>
2013-04-13 03:54:05 -0700

    input store?

--- input_store.py
+++ input_store.py
@@ -53,8 +53,7 @@ def main():
     thread.start()
     threads.append(thread)
 
-    print("MUPEN64!")
-    emulator = Popen(["mupen64plus", "roms/Mario Kart 64 (USA).n64"])
+    #emulator = Popen(["/usr/games/mupen64plus", "roms/Mario Kart 64 (USA).n64"])
 
     while ecodes.KEY_LEFTSHIFT not in events:
         sleep(.1)
