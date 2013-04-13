29460dd29a12fa3a738d032be4c18b1b037463e0
Scott Breyfogle <swb02009@mymail.pomona.edu>
2013-04-13 02:03:34 -0700

    Better input recording

--- input_store.py
+++ input_store.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python2
-from evdev import InputDevice, categorize, ecodes
+from evdev import InputDevice, categorize, ecodes, list_devices
 from re import search
 from time import time, sleep
-import thread
+from threading import Thread
+from subprocess import Popen
 import pickle
 import os
 import sys
 import Image
 import json
 
-#dev = InputDevice('/dev/input/event4')
-
+input_dict = {}
 events = set([]) #A set that will contain all keys currently being held down
+global running
+running = True
+global threads
+threads = []
 
 def read_input():
+    dev = None
+    for dev in map(InputDevice, list_devices()):
+        if dev.name.lower().find('keyboard') > 0:
+            break
+    print dev.name
     for event in dev.read_loop():
         if event.type == ecodes.EV_KEY:
             (key,state) = event.code, event.value
@@ -23,27 +32,53 @@ def read_input():
             elif state == 0: #Key is released
                 if key in events:
                     events.remove(key) #Removes a released key from events
+        if not running:
+            break
  
-def store(filename, image_dir):
-    thread.start_new_thread(read_input, ())
-    input_dict = {}
+def store(image_dir):
     previous_time = time()
     current_time = 0
-    try:
-        while True:#(time() - previous_time) < 5:
-            current_time = time() - previous_time
-            os.system("scrot -u %s/%.4fs.png" % (image_dir,current_time))
-            #current_img = Image.open("%ds.png" % current_time)
-            input_dict[current_time] = ("%s/%.4fs.png" % (image_dir,current_time),list(events))
-            sleep(0.1)
-    except KeyboardInterrupt:
-        f = open(filename,"w")
-        json.dump(input_dict,f)
+    while running:
+        current_time = time() - previous_time
+        img_file = "%s/%.4fs.png" % (image_dir,current_time) 
+        fail = os.system("scrot -u {}".format(img_file))
+        if fail:
+            break
+        print(result)
+        input_dict[current_time] = (img_file,list(events))
+        sleep(.1)
+
+def main():
+    thread = Thread(target=read_input)
+    thread.start()
+    threads.append(thread)
+
+    emulator = Popen(["mupen64plus", "roms/Mario Kart 64 (USA).n64"])
+
+    while ecodes.KEY_LEFTSHIFT not in events:
+        sleep(.1)
+    
+    thread = Thread(target=store, args=(sys.argv[2],))
+    thread.start()
+    threads.append(thread)
+
+    return emulator
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        global dev
-        dev = InputDevice(sys.argv[1])
-        store(sys.argv[2], sys.argv[3])
+    if len(sys.argv) == 3:
+        emulator = None
+        try:
+            emulator = main()
+            while True:
+                sleep(1)
+        except:
+            running = False
+            for thread in threads:
+                "Joining.."
+                thread.join()
+            f = open(sys.argv[1], "w")
+            json.dump(input_dict,f)
+            emulator.terminate()
+            raise
     else:
-        print "Usage: ./input_store.py device training_file.json image_directory"
+        print "Usage: ./input_store.py training_file.json image_directory"
